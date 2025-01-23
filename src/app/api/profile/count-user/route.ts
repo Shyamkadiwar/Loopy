2c2205515e927d26e06c957f62bce5169d50289d
Shyamkadiwar <shyamkadiwar17@gmail.com>
2025-01-23 11:02:00 +0530

    get user with id and count

---
+++ src/app/api/profile/count-user/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+
+export async function GET(request: Request) {
+    const session = await getServerSession(authOptions)
+    if(!session?.user){
+        return new Response(
+            JSON.stringify({
+              success: false,
+              message: "User not authenticated",
+            }),
+            { status: 401, headers: { "Content-Type": "application/json" } }
+        );
+    }
+
+    const userCount = await prisma.user.count()
+
+    if(!userCount){
+        return new Response(
+            JSON.stringify({
+              success: false,
+              message: "Error while counting users",
+            }),
+            { status: 500, headers: { "Content-Type": "application/json" } }
+        );
+    }
+    
+    return new Response(
+        JSON.stringify({
+          success: true,
+          message: "User count fetched successfully",
+          data : userCount
+        }),
+        { status: 200, headers: { "Content-Type": "application/json" } }
+    );
+}
---
+++ src/app/api/profile/get-selected-user/[userId]/route.ts
@@ -0,0 +1,62 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+
+export async function GET(request: Request, {params} : {params : {userId : string}}){
+    try {
+        const session = await getServerSession(authOptions)
+        if(!session?.user){
+            return new Response(
+                JSON.stringify({
+                  success: false,
+                  message: "User not authenticated",
+                }),
+                { status: 401, headers: { "Content-Type": "application/json" } }
+            );
+        }
+    
+        const userId = params.userId
+    
+        const details = await prisma.user.findUnique({
+            where : {id : userId},
+            select : {
+                name : true,
+                email : true,
+                image : true,
+                bio : true,
+                reputation_points : true,
+                created_at : true,
+                interest : true,
+                links :true,
+            }
+        })
+    
+        if (!details) {
+            return new Response(
+                JSON.stringify({
+                success: false,
+                message: "Error while fetching user details",
+                }),
+                { status: 401, headers: { "Content-Type": "application/json" } }
+            );
+        }
+    
+        return new Response(
+            JSON.stringify({
+            success: true,
+            message: "User details fetched successfully",
+            data : details
+            }),
+            { status: 200, headers: { "Content-Type": "application/json" } }
+        );
+    } catch (error) {
+        console.error("Error while fetching user details:", error);
+        return new Response(
+            JSON.stringify({
+                success: false,
+                message: "Internal server error",
+            }),
+            { status: 500, headers: { "Content-Type": "application/json" } }
+        );
+    }
+}
